f8b6ef2e07d8d2b4c336f98dd4f4515105b19b75
SkyVerge <SkyVerge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 15:29:41 +0000

    Tagging 1.4.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-sequential-order-numbers/trunk@1086480 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-sequential-order-numbers.php
+++ woocommerce-sequential-order-numbers.php
@@ -5,7 +5,9 @@
  * Description: Provides sequential order numbers for WooCommerce orders
  * Author: SkyVerge
  * Author URI: http://www.skyverge.com
- * Version: 1.3.4
+ * Version: 1.4.0
+ * Text Domain: woocommerce-sequential-order-numbers
+ * Domain Path: /i18n/languages/
  *
  * Copyright: (c) 2012-2013 SkyVerge, Inc. (info@skyverge.com)
  *
@@ -36,7 +38,7 @@ $GLOBALS['wc_seq_order_number'] = new WC_Seq_Order_Number();
 class WC_Seq_Order_Number {
 
 	/** version number */
-	const VERSION = "1.3.4";
+	const VERSION = "1.4.0";
 
 	/** version option name */
 	const VERSION_OPTION_NAME = "woocommerce_seq_order_number_db_version";
@@ -189,12 +191,12 @@ class WC_Seq_Order_Number {
 	 * @param string $order_number the order id with a leading hash
 	 * @param WC_Order $order the order object
 	 *
-	 * @return string custom order number, with leading hash
+	 * @return string custom order number, with leading hash for < WC 2.3
 	 */
 	public function get_order_number( $order_number, $order ) {
 
 		if ( $order->order_number ) {
-			return '#' . $order->order_number;
+			return ( self::is_wc_version_gte_2_3() ? '' : '#' ) . $order->order_number;
 		}
 
 		return $order_number;
@@ -463,6 +465,17 @@ class WC_Seq_Order_Number {
 
 
 	/**
+	 * Returns true if the installed version of WooCommerce is 2.3 or greater
+	 *
+	 * @since 1.4.0
+	 * @return boolean true if the installed version of WooCommerce is 2.3 or greater
+	 */
+	public static function is_wc_version_gte_2_3() {
+		return self::get_wc_version() && version_compare( self::get_wc_version(), '2.3', '>=' );
+	}
+
+
+	/**
 	 * Perform a minimum WooCommerce version check
 	 *
 	 * @since 1.3.2
@@ -508,15 +521,41 @@ class WC_Seq_Order_Number {
 		$installed_version = get_option( WC_Seq_Order_Number::VERSION_OPTION_NAME );
 
 		if ( ! $installed_version ) {
-			// initial install, set the order number for all existing orders to the post id
-			$orders = get_posts( array( 'numberposts' => '', 'post_type' => 'shop_order', 'nopaging' => true, 'post_status' => self::is_wc_version_gte_2_2() ? 'any' : 'publish' ) );
-			if ( is_array( $orders ) ) {
-				foreach( $orders as $order ) {
-					if ( '' == get_post_meta( $order->ID, '_order_number', true ) ) {
-						add_post_meta( $order->ID, '_order_number', $order->ID );
+
+			// initial install, set the order number for all existing orders to the post id:
+			//  page through the "publish" orders in blocks to avoid out of memory errors
+			$offset         = (int) get_option( 'wc_sequential_order_numbers_install_offset', 0 );
+			$posts_per_page = 500;
+
+			do {
+
+				// initial install, set the order number for all existing orders to the post id
+				$order_ids = get_posts( array( 'post_type' => 'shop_order', 'fields' => 'ids', 'offset' => $offset, 'posts_per_page' => $posts_per_page, 'post_status' => self::is_wc_version_gte_2_2() ? 'any' : 'publish' ) );
+
+				// some sort of bad database error: deactivate the plugin and display an error
+				if ( is_wp_error( $order_ids ) ) {
+					require_once ABSPATH . 'wp-admin/includes/plugin.php';
+					deactivate_plugins( 'woocommerce-sequential-order-numbers/woocommerce-sequential-order-numbers.php' );  // hardcode the plugin path so that we can use symlinks in development
+
+					wp_die( sprintf( __( 'Error activating and installing <strong>WooCommerce Sequential Order Numbers</strong>: %s', 'woocommerce-sequential-order-numbers' ), '<ul><li>' . implode( '</li><li>', $order_ids->get_error_messages() ) . '</li></ul>' ) .
+					        '<a href="' . admin_url( 'plugins.php' ) . '">' . __( '&laquo; Go Back', 'woocommerce-sequential-order-numbers' ) . '</a>' );
+				}
+
+
+				if ( is_array( $order_ids ) ) {
+					foreach( $order_ids as $order_id ) {
+						if ( '' == get_post_meta( $order_id, '_order_number', true ) ) {
+							add_post_meta( $order_id, '_order_number', $order_id );
+						}
 					}
 				}
-			}
+
+				// increment offset
+				$offset += $posts_per_page;
+				// and keep track of how far we made it in case we hit a script timeout
+				update_option( 'wc_sequential_order_numbers_install_offset', $offset );
+
+			} while ( count( $order_ids ) == $posts_per_page );  // while full set of results returned  (meaning there may be more results still to retrieve)
 		}
 
 		if ( $installed_version != WC_Seq_Order_Number::VERSION ) {
